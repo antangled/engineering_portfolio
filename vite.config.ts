f4bbfe27be69fa3785f9ae37c6cc3c24bdb3c6d5
Adam Tang <adamtang@Adams-MacBook-Pro.local>
2025-10-19 15:28:26 -0700

    more file changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/engineering_portfolio/",
   plugins: [react()],
   server: {
     port: 5173,
